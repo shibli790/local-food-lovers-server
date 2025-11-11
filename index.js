536f9d11bc515736bdcee1ae1a7cd7ee70ca3658
shibli790 <shibliahmed444@gmail.com>
2025-11-11 22:27:58 +0600

    recode delete  and authrequired

--- index.js
+++ index.js
@@ -289,7 +289,11 @@ app.post('/favorites/:reviewId', authRequired, async (req, res) => {
 // Favorite remove 
 app.delete('/favorites/:reviewId', authRequired, async (req, res) => {
   try {
-   
+    const reviewId = req.params.reviewId;
+    const r = await Favorites.deleteOne({
+      userEmail: req.user.email,
+      reviewId,
+    });
     res.json({ deleted: r.deletedCount === 1 });
   } catch (e) {
     res.status(400).json({ message: 'Invalid review id' });
