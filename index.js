81045cc83e76983e9a82d6f234410b11521db2a6
shibli790 <shibliahmed444@gmail.com>
2025-11-11 22:26:20 +0600

    delete favorites add

--- index.js
+++ index.js
@@ -289,11 +289,7 @@ app.post('/favorites/:reviewId', authRequired, async (req, res) => {
 // Favorite remove 
 app.delete('/favorites/:reviewId', authRequired, async (req, res) => {
   try {
-    const reviewId = req.params.reviewId;
-    const r = await Favorites.deleteOne({
-      userEmail: req.user.email,
-      reviewId,
-    });
+   
     res.json({ deleted: r.deletedCount === 1 });
   } catch (e) {
     res.status(400).json({ message: 'Invalid review id' });
